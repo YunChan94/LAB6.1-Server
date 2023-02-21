ae09c1ffbf98c74e704c0413c3846a66414136a6
YunChan <juleefang94@gmail.com>
2023-02-21 16:58:43 +0700

    hoan thanh lab7.1

--- Controllers/products.js
+++ Controllers/products.js
@@ -1,20 +1,30 @@
-const Product = require("../models/product");
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
+  const product = new Product(req.body.title);
   product.save();
-  // Product.fetchAll((products) => {
-  //   res.json({ products: products });
-  // });
-  res.json("Add product successful!");
+  res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.json({ products: products });
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
